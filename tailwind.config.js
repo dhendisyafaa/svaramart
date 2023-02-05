36df62192f87302d3264ef823fcf093b607316bf
Dhendi Syafa <dhendisyafa@gmail.com>
2023-02-05 21:51:55 +0700

    chore: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // add this line
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    // if you have other folders, add their paths too, like this:
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navy: "#1F3C42",
+        grey: "#8E8E8E"
+      }
+    },
+  },
+  plugins: [],
+}
