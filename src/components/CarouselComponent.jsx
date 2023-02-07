c418b18bf77df5d083fb04ee6962031fc08db661
Dhendi Syafa <dhendisyafa@gmail.com>
2023-02-07 14:05:11 +0700

    feat: add image for carousel

--- src/components/CarouselComponent.jsx
+++ src/components/CarouselComponent.jsx
@@ -6,6 +6,8 @@ const CarouselComponent = () => {
     const images = [
         '/images/carousel/carousel-1.png',
         '/images/carousel/carousel-2.png',
+        'https://images.unsplash.com/photo-1441986300917-64674bd600d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2hvcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+        'https://images.unsplash.com/photo-1530738472658-404f693bd4b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bWFsbHxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60'
     ]
 
     // const indicators = (index) => (<button className="indicator w-2 h-2">
@@ -65,7 +67,7 @@ const CarouselComponent = () => {
             <Slide {...slideProperties}>
                 {images.map((each, index) => (
                     <div key={index} className='w-full h-40'>
-                        <img src={each} alt="carousel" className='w-full h-40 object-cover rounded-lg shadow-md' />
+                        <img src={each} alt="carousel" className='w-full h-40 object-cover rounded-lg shadow-md' loading='lazy'/>
                     </div>
                 ))}
             </Slide>
