5580588155dd04d6a5241bcd7e9a62da28cb75b6
Dhendi Syafa <dhendisyafa@gmail.com>
2023-02-07 16:58:44 +0700

    feat: add page promo

--- src/pages/promo.js
+++ src/pages/promo.js
@@ -1,13 +1,52 @@
-import Layout from '@/components/Layout'
-import React from 'react'
+import BadgeTitle from '@/components/BadgeTitle'
+import CouponComponent from '@/components/CouponComponent'
+import VoucherComponent from '@/components/VoucherComponent'
+import CardPromo from '@/components/cards/CardPromo'
+import CarouselFull from '@/components/carousel/CarouselFull'
+import SimpleHeader from '@/components/header/SimpleHeader'
+import NavbarComponent from '@/components/navbar/NavbarComponent'
 
 const promo = () => {
   return (
-    <Layout>
-      <div className='h-[70vh] flex justify-center items-center'>
-        <p>COMMING SOON!</p> 
+    <div className='overflow-hidden'>
+      <SimpleHeader titleHeader="Promo" />
+      <CarouselFull />
+      <div className='mt-6'>
+        <BadgeTitle />
+        <div className='mx-5 flex gap-1 mt-5 overflow-x-scroll scrollbar-hide'>
+          <CardPromo />
+          <CardPromo />
+          <CardPromo />
+          <CardPromo />
+        </div>
+        <div className='px-5 text-navy font-bold text-md mt-8 mb-4'>
+          <p className='uppercase'>Voucher Gratis Ongkir Mingguan</p>
+        </div>
+        <div className='mx-5 flex gap-2 mt-5 overflow-x-scroll scrollbar-hide py-2 px-2 w-[1000px]'>
+          <CouponComponent />
+          <CouponComponent />
+        </div>
+        <div className='px-5 text-navy font-bold text-md mt-8 mb-4'>
+          <p className='uppercase'>Voucher Gratis Ongkir Mingguan</p>
+        </div>
+        <div className='px-5 flex flex-col gap-2'>
+          <VoucherComponent />
+          <VoucherComponent />
+          <VoucherComponent />
+          <VoucherComponent />
+        </div>
+        <div className='mt-12'>
+          <BadgeTitle />
+          <div className='mx-5 flex gap-1 mt-5 overflow-x-scroll scrollbar-hide'>
+            <CardPromo />
+            <CardPromo />
+            <CardPromo />
+            <CardPromo />
+          </div>
+        </div>
       </div>
-    </Layout>
+      <NavbarComponent />
+    </div>
   )
 }
 
