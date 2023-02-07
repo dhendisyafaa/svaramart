08aef86812765916a64ef811acc8cb27d4648932
Dhendi Syafa <dhendisyafa@gmail.com>
2023-02-07 16:59:04 +0700

    feat: add badge title component

---
+++ src/components/BadgeTitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BadgeTitle = () => {
+  return (
+    <div className='bg-navy max-w-fit rounded-r-xl py-2 px-5'>
+        <p className='text-white font-semibold'>Promo Akhir Bulan</p>
+    </div>
+  )
+}
+
+export default BadgeTitle
