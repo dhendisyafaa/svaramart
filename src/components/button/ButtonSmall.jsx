8310062af5ad1bf76c3f29ee3c1c1ee45d0f73ce
Dhendi Syafa <dhendisyafa@gmail.com>
2023-02-07 13:49:43 +0700

    style: edit style button small

--- src/components/button/ButtonSmall.jsx
+++ src/components/button/ButtonSmall.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ButtonSmall = (props) => {
   return (
-    <button className='text-[8px] bg-navy text-white rounded-lg max-w-fit px-2 text-center py-1'>{props.innerText}</button>
+    <button className='text-[8px] font-medium hover:bg-[#102a30] bg-navy text-white rounded-lg max-w-fit px-2 text-center py-1'>{props.innerText}</button>
   )
 }
 
