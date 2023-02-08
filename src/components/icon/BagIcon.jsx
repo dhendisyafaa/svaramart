f2afbca43ae0c5ae94ee88223c7d6260fadd56d8
Dhendi Syafa <dhendisyafa@gmail.com>
2023-02-08 16:26:09 +0700

    feat: create page bag

--- src/components/icon/BagIcon.jsx
+++ src/components/icon/BagIcon.jsx
@@ -1,16 +1,19 @@
+import Link from 'next/link'
 import React from 'react'
 
 const BagIcon = (props) => {
     return (
-        <svg width={26} height={26} fill="none">
-            <path
-                d="M6.5 2.167L3.25 6.5v15.167a2.167 2.167 0 002.167 2.166h15.166a2.167 2.167 0 002.167-2.166V6.5L19.5 2.167h-13zM3.25 6.5h19.5M17.333 10.833a4.333 4.333 0 11-8.666 0"
-                stroke={props.color}
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            />
-        </svg>
+        <Link href="/bag">
+            <svg width={26} height={26} fill="none">
+                <path
+                    d="M6.5 2.167L3.25 6.5v15.167a2.167 2.167 0 002.167 2.166h15.166a2.167 2.167 0 002.167-2.166V6.5L19.5 2.167h-13zM3.25 6.5h19.5M17.333 10.833a4.333 4.333 0 11-8.666 0"
+                    stroke={props.color}
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                />
+            </svg>
+        </Link>
     )
 }
 
---
+++ src/pages/bag.js
@@ -0,0 +1,16 @@
+import SimpleHeader from "@/components/header/SimpleHeader"
+
+const bag = () => {
+    return (
+        <>
+            <SimpleHeader
+                titleHeader="Bag"
+            />
+            <div className='h-[70vh] flex justify-center items-center'>
+                <p>COMMING SOON!</p>
+            </div>
+        </>
+    )
+}
+
+export default bag
