0a2c8c953a970a9ee4d53ecc496e6c1fa72baaf6
Dhendi Syafa <dhendisyafa@gmail.com>
2023-02-08 16:26:59 +0700

    style: fix shadow for navbar

--- src/components/navbar/NavbarComponent.jsx
+++ src/components/navbar/NavbarComponent.jsx
@@ -6,7 +6,7 @@ const NavbarComponent = () => {
   const router = useRouter();
 
   return (
-    <div className="navbar grid grid-cols-4 fixed bottom-0 items-center w-full py-2.5 rounded-t-xl bg-white shadow-[0_30px_30px_20px_rgba(0,0,0,1)] z-30">
+    <div className="navbar grid grid-cols-4 fixed bottom-0 items-center w-full py-2.5 rounded-t-xl bg-white shadow-[0px_10px_30px_0px_rgba(0,0,0,1)] z-30">
       <BerandaIcon navbarName={'Beranda'} href={'/'} selected={router.pathname === '/' ? true : false} />
       <PromoIcon navbarName={'Promo'} href={'/promo'} selected={router.pathname === '/promo' ? true : false} />
       <OrderIcon navbarName={'Order'} href={'/order'} selected={router.pathname === '/order' ? true : false} />
