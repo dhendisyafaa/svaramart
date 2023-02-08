94e5042243db2987086fe9bfae79216c5c9b3a7a
Dhendi Syafa <dhendisyafa@gmail.com>
2023-02-08 11:45:20 +0700

    style: change structure component page home

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import SearchBar from '@/components/SearchBar'
 import ButtonSmall from '@/components/button/ButtonSmall'
 import CardProduct from '@/components/cards/CardProduct'
 import CarouselComponent from '@/components/carousel/CarouselComponent'
-import BagIcon from '@/components/logo/BagIcon'
+import BagIcon from '@/components/icon/BagIcon'
 import { Inter } from '@next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -13,41 +13,33 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <Layout>
-      <div className='flex flex-col gap-8 h-[1500px]'>
-        <div className='flex flex-col gap-7'>
-          <div className='flex flex-col gap-5'>
-            <div className='flex gap-2 items-center '>
-              <SearchBar />
-              <BagIcon color="#1F3C42" />
-            </div>
-            <CarouselComponent />
-          </div>
+      <div className='h-[1500px]'>
+        <div className='flex gap-2 items-center mb-5'>
+          <SearchBar />
+          <BagIcon color="#1F3C42" />
+        </div>
+        <div className='mb-6'>
+          <CarouselComponent />
+        </div>
+        <div className='mb-10'>
           <CategoryComponent />
         </div>
-        <div className='flex flex-col'>
-          <div>
-            <p className='font-semibold text-sm'>Produk terakhir dilihat</p>
-          </div>
-          <div className='flex gap-2 overflow-x-scroll scrollbar-hide py-3'>
-            <CardProduct />
-            <CardProduct />
-            <CardProduct />
-            <CardProduct />
-            <CardProduct />
-          </div>
+        <p className='font-semibold text-sm'>Produk terakhir dilihat</p>
+        <div className='flex gap-1 lg:gap-2 overflow-x-scroll scrollbar-hide py-3 mb-8'>
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
         </div>
-        <div className='flex flex-col'>
-          <div className='flex items-center justify-between'>
-            <p className='font-semibold text-sm'>Produk di sekitarmu!</p>
-            <ButtonSmall innerText="Lihat Semua"/>
-          </div>
-          <div className='flex gap-2 overflow-x-scroll scrollbar-hide py-3'>
-            <CardProduct />
-            <CardProduct />
-            <CardProduct />
-            <CardProduct />
-            <CardProduct />
-          </div>
+        <div className='flex items-center justify-between'>
+          <p className='font-semibold text-sm'>Produk di sekitarmu!</p>
+          <ButtonSmall innerText="Lihat Semua" />
+        </div>
+        <div className='flex gap-1 lg:gap-2 overflow-x-scroll scrollbar-hide py-3'>
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
+          <CardProduct />
         </div>
       </div>
     </Layout>
