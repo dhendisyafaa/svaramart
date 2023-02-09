aa49d447659ecb98353337f0086acc9639df83c1
Dhendi Syafa <dhendisyafa@gmail.com>
2023-02-09 15:35:38 +0700

    feat: modify simple header

--- src/components/header/SimpleHeader.jsx
+++ src/components/header/SimpleHeader.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import NotifBell from '../icon/NotifBell'
+import BagIcon from '../icon/BagIcon'
+import HamburgerMenu from '../icon/HamburgerMenu'
 
 const SimpleHeader = (props) => {
   return (
-    <div className='bg-navy px-5 py-4'>
-        <p className='text-white font-medium text-md'>{props.titleHeader}</p>
+    <div className='bg-navy px-5 py-3 flex items-center justify-between'>
+      <p className='text-white font-medium text-md'>{props.titleHeader}</p>
+      <div className='flex items-center gap-1'>
+        <NotifBell />
+        <BagIcon color="#fff" />
+        <HamburgerMenu />
+      </div>
     </div>
   )
 }
