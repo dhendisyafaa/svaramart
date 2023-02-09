cd5e2889eebe96574a8cfdf8ea3b4061953c6ae2
Dhendi Syafa <dhendisyafa@gmail.com>
2023-02-09 14:38:27 +0700

    feat: add chaticon and hamburger menu

--- src/components/header/LargeScreenHeader.jsx
+++ src/components/header/LargeScreenHeader.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import SearchBar from '../SearchBar'
 import SvaraMartText from '../logo/SvaraMartText'
 import BagIcon from '../icon/BagIcon'
+import ChatIcon from '../icon/ChatIcon'
+import HamburgerMenu from '../icon/HamburgerMenu'
 
 const LargeScreenHeader = () => {
     return (
-        <div className='grid grid-cols-[50%_auto] bg-navy px-20 py-4'>
+        <div className='grid grid-cols-[50%_auto] items-center bg-navy px-20 py-4'>
             <SvaraMartText />
-            <div className='flex gap-6 items-center px-4'>
+            <div className='flex gap-3 items-center px-4'>
                 <SearchBar />
                 <BagIcon color="#fff" />
+                <ChatIcon color="#fff"/>
+                <HamburgerMenu/>
             </div>
         </div>
     )
