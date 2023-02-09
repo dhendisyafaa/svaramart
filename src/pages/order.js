30cae6093efdd7a4a9d51f42b48117dd92804efb
Dhendi Syafa <dhendisyafa@gmail.com>
2023-02-09 16:23:44 +0700

    feat: add tab from headless ui for order page

--- src/pages/order.js
+++ src/pages/order.js
@@ -1,13 +1,117 @@
-import Layout from '@/components/Layout'
-import React from 'react'
+import HamburgerMenu from '@/components/icon/HamburgerMenu'
+import NavbarComponent from '@/components/navbar/NavbarComponent'
+import { Tab } from '@headlessui/react'
+import Image from 'next/image'
+import avatar from '../../public/images/icon/avatar.png'
+import logoPromo from '../../public/images/icon/logo promo.png'
+import classNames from 'classnames'
 
 const order = () => {
     return (
-        <Layout>
-            <div className='h-[70vh] flex justify-center items-center'>
-                <p>COMMING SOON!</p>
+        <>
+            <div className='bg-navy'>
+                <div className='w-full flex justify-end px-3 py-1'>
+                    <HamburgerMenu />
+                </div>
+                <div className='flex items-center gap-2 px-5 pb-7'>
+                    <Image
+                        src={avatar} />
+                    <div>
+                        <p className='font-normal text-md text-white'>namaemail@gmail.com</p>
+                        <div className='flex items-center gap-1'>
+                            <svg width={10} height={10} fill="none">
+                                <g
+                                    clipPath="url(#prefix__clip0_46_1445)"
+                                    stroke="#fff"
+                                    strokeWidth={1.5}
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                >
+                                    <path d="M8.75 4.167C8.75 7.083 5 9.583 5 9.583s-3.75-2.5-3.75-5.416a3.75 3.75 0 017.5 0z" />
+                                    <path d="M5 5.417a1.25 1.25 0 100-2.5 1.25 1.25 0 000 2.5z" />
+                                </g>
+                                <defs>
+                                    <clipPath id="prefix__clip0_46_1445">
+                                        <path fill="#fff" d="M0 0h10v10H0z" />
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                            <p className='text-[10px] text-white'>alamatmu Lorem ipsum dolor sit amet</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='bg-white rounded-t-3xl'>
+                    <Tab.Group>
+                        <Tab.List className="py-8 px-6 gap-8 flex">
+                            <Tab className={({ selected }) =>
+                                classNames(
+                                    '',
+                                    selected
+                                        ? 'border-b-navy border-b-2 font-medium'
+                                        : 'text-grey'
+                                )
+                            }>
+                                <p>Pesanan</p>
+                            </Tab>
+                            <Tab  className={({ selected }) =>
+                                classNames(
+                                    '',
+                                    selected
+                                        ? 'border-b-navy border-b-4 font-medium'
+                                        : 'text-grey'
+                                )
+                            }>
+                                <p>Transaksi</p>
+                            </Tab>
+                        </Tab.List>
+                        <Tab.Panels className="px-6">
+                            <Tab.Panel>
+                                <div>
+                                    <div className='mb-8'>
+                                        <p className='text-sm'>Status Pesanan</p>
+                                    </div>
+                                    <div className='flex justify-around'>
+                                        <div className='text-center flex flex-col gap-1'>
+                                            <Image
+                                                src={logoPromo}
+                                                className='mx-auto'
+                                            />
+                                            <p className='font-medium text-[10px]'>Belum Bayar</p>
+                                        </div>
+                                        <div className='text-center flex flex-col gap-1'>
+                                            <Image
+                                                src={logoPromo}
+                                                className='mx-auto'
+                                            />
+                                            <p className='font-medium text-[10px]'>Diproses</p>
+                                        </div>
+                                        <div className='text-center flex flex-col gap-1'>
+                                            <Image
+                                                src={logoPromo}
+                                                className='mx-auto'
+                                            />
+                                            <p className='font-medium text-[10px]'>Dikirim</p>
+                                        </div>
+                                        <div className='text-center flex flex-col gap-1'>
+                                            <Image
+                                                src={logoPromo}
+                                                className='mx-auto'
+                                            />
+                                            <p className='font-medium text-[10px]'>Ulasan</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Tab.Panel>
+                            <Tab.Panel>
+                                <p>tab 2</p>
+                            </Tab.Panel>
+
+                        </Tab.Panels>
+                    </Tab.Group>
+                </div>
             </div>
-        </Layout>
+            <NavbarComponent />
+        </>
     )
 }
 
