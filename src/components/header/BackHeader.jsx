a79f8d4dd5ef97dead0a4a75148a1fff409c21b8
Dhendi Syafa <dhendisyafa@gmail.com>
2023-02-10 14:15:28 +0700

    feat: add back header

---
+++ src/components/header/BackHeader.jsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import React from 'react'
+
+const BackHeader = (props) => {
+    return (
+        <div className='bg-navy px-5 py-3 flex items-center gap-2'>
+            <Link href='/'>
+                <svg width={24} height={24} fill="none">
+                    <path
+                        d="M19 12H5M12 19l-7-7 7-7"
+                        stroke="#fff"
+                        strokeWidth={2}
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    />
+                </svg>
+            </Link>
+            <p className='text-white font-medium text-md'>{props.titleHeader}</p>
+        </div>
+    )
+}
+
+export default BackHeader
