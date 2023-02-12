88069dc6e396ec9c36e1e5f19ce900604c31ef21
Dhendi Syafa <dhendisyafa@gmail.com>
2023-02-12 12:11:16 +0700

    feat: add transition for navbar

--- src/components/navbar/NavbarComponent.jsx
+++ src/components/navbar/NavbarComponent.jsx
@@ -1,16 +1,23 @@
 import { useRouter } from 'next/router';
 import { BerandaIcon, OrderIcon, PromoIcon, WishlistIcon } from './NavbarIcon';
+import { motion } from 'framer-motion';
 
 const NavbarComponent = () => {
   const router = useRouter();
 
   return (
-    <div className="navbar grid grid-cols-4 fixed bottom-0 items-center w-full py-2.5 rounded-t-xl bg-white shadow-[0px_10px_30px_0px_rgba(0,0,0,1)] z-30">
+    <motion.div
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      transition={{
+        duration: 1,
+        delay: 1,
+      }} className="navbar grid grid-cols-4 fixed bottom-0 items-center w-full py-2.5 rounded-t-xl bg-white shadow-[0px_10px_30px_0px_rgba(0,0,0,1)] z-30">
       <BerandaIcon navbarName={'Beranda'} href={'/'} selected={router.pathname === '/' ? true : false} />
       <PromoIcon navbarName={'Promo'} href={'/promo'} selected={router.pathname === '/promo' ? true : false} />
       <WishlistIcon navbarName={'Wishlist'} href={'/wishlist'} selected={router.pathname === '/wishlist' ? true : false} />
       <OrderIcon navbarName={'Order'} href={'/order'} selected={router.pathname === '/order' ? true : false} />
-    </div>
+    </motion.div>
   )
 }
 
