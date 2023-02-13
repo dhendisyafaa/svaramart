10ede49a526178f176887fcab6faa5e4d6efdebf
Dhendi Syafa <dhendisyafa@gmail.com>
2023-02-13 19:28:22 +0700

    feat: add settings for pwa

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,35 @@
+import Head from 'next/head';
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
+  <Head>
+    <meta charSet="utf-8" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+    <meta
+      name="viewport"
+      content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+    />
+    <meta name="description" content="Online Shop" />
+    <meta name="keywords" content="Online Shop" />
+    <title>SvaraMart</title>
+
+    <link rel="manifest" href="/manifest.json" />
+    <link
+      href="/icons/favicon-16x16.png"
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+    />
+    <link
+      href="/icons/favicon-32x32.png"
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+    />
+    <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+    <meta name="theme-color" content="#ffffff" />
+  </Head>
   return <Component {...pageProps} />
 }
 
