9643bdd7e81176a4160df99f5fb30624fafbe2aa
Dhendi Syafa <dhendisyafa@gmail.com>
2023-02-13 12:00:18 +0700

    style: remove name class

--- src/components/navbar/NavbarComponent.jsx
+++ src/components/navbar/NavbarComponent.jsx
@@ -12,7 +12,7 @@ const NavbarComponent = () => {
       transition={{
         duration: 1,
         delay: 1,
-      }} className="navbar grid grid-cols-4 fixed bottom-0 items-center w-full py-2.5 rounded-t-xl bg-white shadow-[0px_5px_10px_0px_rgba(0,0,0,1)] z-30">
+      }} className="grid grid-cols-4 fixed bottom-0 items-center w-full py-2.5 rounded-t-xl bg-white shadow-[0px_5px_10px_0px_rgba(0,0,0,1)] z-30">
       <BerandaIcon navbarName={'Beranda'} href={'/'} selected={router.pathname === '/' ? true : false} />
       <PromoIcon navbarName={'Promo'} href={'/promo'} selected={router.pathname === '/promo' ? true : false} />
       <WishlistIcon navbarName={'Wishlist'} href={'/wishlist'} selected={router.pathname === '/wishlist' ? true : false} />
