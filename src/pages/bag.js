ea42a6642bce9f9b1db71d33be6342a1a182e20e
Dhendi Syafa <dhendisyafa@gmail.com>
2023-02-14 21:18:01 +0700

    feat: create page checkout

--- src/pages/bag.js
+++ src/pages/bag.js
@@ -6,7 +6,7 @@ const bag = () => {
     return (
         <>
             <BackHeader titleHeader='Bag' styleIcon="hidden" />
-            <div className="h-[1700px] lg:px-18 px-5 md:grid md:grid-cols-[auto_30%]">
+            <div className="h-[1700px] lg:px-18 md:grid md:grid-cols-[auto_30%]">
                 <div className="">
                     <ItemBag />
                     <ItemBag />
@@ -18,14 +18,14 @@ const bag = () => {
                         <p className="font-medium text-lg text-[#2c2c2c]">Total</p>
                         <p className="font-medium text-lg text-navy">Rp95.000</p>
                     </div>
-                    <button type="" className="bg-navy text-white rounded-3xl w-full py-2 font-semibold text-lg">Checkout (100)</button>
+                    <Link href="/checkout" className="bg-navy text-white text-center rounded-3xl w-full py-2 font-semibold text-lg hover:bg-[#183237] active:bg-[#13292e]">Checkout (100)</Link>
                 </div>
                 <div className="md:hidden grid grid-cols-[auto_35%] fixed bottom-0 items-center w-full h-[8vh] bg-white z-30">
-                    <div className='py-2.5 px-6'>
+                    <div className='py-2.5 px-5'>
                         <p className='font-normal text-xs'>Total Belanja</p>
                         <p className='font-bold text-md text-navy'>Rp. 129.950</p>
                     </div>
-                    <Link href="/checkout" className='bg-navy text-white h-full flex justify-center items-center font-bold text-sm'>Checkout (100)</Link>
+                    <Link href="/checkout" className='bg-navy hover:bg-[#183237] active:bg-[#13292e] text-white h-full flex justify-center items-center text-center font-bold text-sm'>Checkout (100)</Link>
                 </div>
             </div>
         </>
